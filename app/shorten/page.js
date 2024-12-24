4ac47f1cbe12e661d5195415f6a2a5f6a3c862a5
Priyanshu Anand <anandpriyanshu12@gmail.com>
2024-12-24 17:59:29 +0530

    Create shorten page and connect with mongodb

--- app/shorten/page.js
+++ app/shorten/page.js
@@ -1,5 +1,6 @@
 "use client"
 
+import Link from 'next/link'
 import React, { useState } from 'react'
 
 const Shorten = () => {
@@ -7,6 +8,34 @@ const Shorten = () => {
     const [url, seturl] = useState("")
     const [shorturl, setshorturl] = useState('')
     const [generated, setgenerated] = useState(false)
+
+    const generate = () => {
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        const raw = JSON.stringify({
+            "url": url,
+            "shorturl": shorturl
+        });
+
+        const requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow"
+        };
+
+        fetch("/api/generate", requestOptions)
+            .then((response) => response.json())
+            .then((result) => {
+                setgenerated(`${process.env.NEXT_PUBLIC_HOST}/${shorturl}`)
+                seturl('')
+                setshorturl('')
+                console.log(result)
+                alert(result.message)
+            })
+            .catch((error) => console.error(error));
+    }
     return (
         <>
             <div className='mx-auto max-w-lg bg-blue-200 p-6 rounded-lg flex flex-col gap-4 my-12'>
@@ -15,8 +44,12 @@ const Shorten = () => {
                 <div className='flex flex-col gap-2'>
                     <input className='p-2 focus:outline-green-200' type='text' placeholder='Enter your URL' value={url} onChange={(e) => seturl(e.target.value)} />
                     <input className='p-2 focus:outline-green-200' type='text' placeholder='Enter your preferred short URL text' value={shorturl} onChange={(e) => setshorturl(e.target.value)} />
-                    <button className='bg-blue-600 rounded-lg p-2 font-bold text-white my-2'>Generate</button>
+                    <button className='bg-blue-600 rounded-lg p-2 font-bold text-white my-2' onClick={generate}>Generate</button>
                 </div>
+
+                {generated && <code>
+                    Your Link: <Link target="_blank" href={generated}>{generated}</Link>
+                </code>}
             </div>
         </>
     )
