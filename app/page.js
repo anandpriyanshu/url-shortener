6b0bdca19b009dd4bdaa4f1a5de271e264ac209c
Priyanshu Anand <anandpriyanshu12@gmail.com>
2024-12-24 15:07:52 +0530

    Create main page. Frontend part finished

--- app/page.js
+++ app/page.js
@@ -1,7 +1,27 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <h1>HI I am main page</h1>
+    <>
+      <main className="bg-purple-50">
+        <section className="grid grid-cols-2 h-[50vh]">
+          <div className="flex flex-col gap-3 justify-center items-center">
+            <p className="text-2xl font-bold">The best URL shortener in the Market </p>
+            <p> We are the most straightfoward URL Shortener in the world. </p>
+            <div className=' flex gap-3'>
+              <Link href="/generate"><button className='bg-blue-600 rounded-lg p-2 font-bold text-white'>Try now</button></Link>
+              <Link href="/github"><button className='bg-blue-600 rounded-lg p-2 font-bold text-white'>Github</button></Link>
+            </div>
+          </div>
+          <div className="flex justify-start relative">
+            <Image className="mix-blend-darken" src={"/vector.jpg"} fill={true} />
+          </div>
+
+
+
+        </section>
+      </main>
+    </>
   );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <nav className='h-16 flex justify-between px-4 items-center bg-blue-500 text-white'>
+        <nav className='h-16 flex justify-between px-4 items-center bg-blue-400 text-white'>
             <div className="logo font-bold text-lg">LinkShortner</div>
             <ul className='flex gap-6 justify-center items-center'>
                 <Link href='/'>Home</Link>
@@ -11,10 +11,10 @@ const Navbar = () => {
                 <Link href='/generate'>Shorten</Link>
                 <Link href='/contact'>Contact Us</Link>
 
-                <li className=' flex gap-3'>
+                <div className=' flex gap-3'>
                     <Link href="/generate"><button className='bg-blue-600 rounded-lg p-2 font-bold'>Try now</button></Link>
                     <Link href="/github"><button className='bg-blue-600 rounded-lg p-2 font-bold'>Github</button></Link>
-                </li>
+                </div>
             </ul >
         </nav >
     )
